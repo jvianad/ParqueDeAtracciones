ce69bf98c26edc80ba2e335261a218983ea1f8b6
jvianad <jeanvianademares@gmail.com>
2023-12-18 16:59:55 -0500

    Fase inicial del proyecto, se crea conexion a base de datos

---
+++ src/main/java/com/parque/ParqueDeAtracciones/ParqueDeAtraccionesApplication.java
@@ -0,0 +1,13 @@
+package com.parque.ParqueDeAtracciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueDeAtraccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueDeAtraccionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parque/ParqueDeAtracciones/ParqueDeAtraccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parque.ParqueDeAtracciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDeAtraccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
